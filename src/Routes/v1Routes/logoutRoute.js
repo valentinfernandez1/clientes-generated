d5f1879ee0d1cf87b7485d900f6b5bd1268e296e
Valentin Fernandez <tinchofernandez8@gmail.com>
2022-10-20 18:04:57 -0300

    cambio

--- src/Routes/v1Routes/logoutRoute.js
+++ src/Routes/v1Routes/logoutRoute.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
-	import validateAuth from "../../middlewares/validateAuth.js";
+import validateAuth from "../../middlewares/validateAuth.js";
 
 const router = Router();
 
-router.get("/:id",
-	[validateAuth],
-  (req, res)=>{res.send('NOT IMPLEMENTED')});
-
+router.get("/logout/:id", [validateAuth], (req, res) => {
+  res.send("NOT IMPLEMENTED");
+});
 
 export default router;
